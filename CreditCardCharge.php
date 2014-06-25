70aff56b181228c2c35edbfae2d70355d9db3b7f
rmossuto@merituspayment.com <rmossuto@merituspayment.com>
2014-06-25 11:24:49 -0700

    Remove whitespace from line #3

--- CreditCardCharge.php
+++ CreditCardCharge.php
@@ -1,6 +1,6 @@
 <?PHP
 //Specify URL + Field Names + Values	
-$postURL = "https://webservice.paymentxp.com/wh/webhost.aspx?							    	MerchantID=10012&MerchantKey=C22A63EE-2E7A-4ACE-96AC-0958DC8D953F&TransactionType=CreditCardCharge&CardNumber=4111111111111111&ExpirationDateMMYY=1218&TransactionAmount=1.00&BillingNameFirst=John&BillingNameLast=Doe";
+$postURL = "https://webservice.paymentxp.com/wh/webhost.aspx?MerchantID=10012&MerchantKey=C22A63EE-2E7A-4ACE-96AC-0958DC8D953F&TransactionType=CreditCardCharge&CardNumber=4111111111111111&ExpirationDateMMYY=1218&TransactionAmount=1.00&BillingNameFirst=John&BillingNameLast=Doe";
 	//See http://php.net/manual/en/book.curl.php for a list of available cURL commands	
 	$request = curl_init($postURL); 
         curl_setopt($request, CURLOPT_HEADER, 0);
