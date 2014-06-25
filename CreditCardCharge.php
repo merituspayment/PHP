b306e1a5b66357d16a47582f11ca000a0c4cf78d
rmossuto@merituspayment.com <rmossuto@merituspayment.com>
2014-06-25 11:13:50 -0700

    New Project

--- CreditCardCharge.php
+++ CreditCardCharge.php
@@ -1,5 +1,5 @@
 <?PHP
-//POST URL including field names and field values 	
+//Specify URL + Field Names + Values	
 $postURL = "https://webservice.paymentxp.com/wh/webhost.aspx?							    	MerchantID=10012&MerchantKey=C22A63EE-2E7A-4ACE-96AC-0958DC8D953F&TransactionType=CreditCardCharge&CardNumber=4111111111111111&ExpirationDateMMYY=1218&TransactionAmount=1.00&BillingNameFirst=John&BillingNameLast=Doe";
 	//See http://php.net/manual/en/book.curl.php for a list of available cURL commands	
 	$request = curl_init($postURL); 
